82810622630890418d538a5ab6f13c9bd9e3ec20
Aditya Manoj Menon <143031124+AdityaManojMenon@users.noreply.github.com>
2023-12-09 23:53:08 -0500

    Create Octal.cpp
    Write a program, named Octal/main.cpp that takes a positive integer from standard input and writes three values to standard output: the number in decimal, in hexadecimal and in octal (each on their own line).
    We strongly recommend consulting the C++ documentation for suggestions on how to modify the std::cout stream to make this happen.

---
+++ Octal.cpp
@@ -0,0 +1,11 @@
+//working file
+#include <iostream>
+
+int main() {
+  int value{0};
+  std::cin >> value;
+  std::cout << std::dec << value << std::endl;
+  std::cout << std::hex << value << std::endl;
+  std::cout << std::oct << value << std::endl;
+  return 0;
+}
